7236aebb41a46434f2bda1e47a648fe6eb736deb
Kyle <k00benne@gmail.com>
2021-03-07 20:56:31 -0500

    Tidy up gitignore & some new packages

--- gql/src/index.ts
+++ gql/src/index.ts
@@ -1,12 +1,21 @@
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloError, ApolloServer } from 'apollo-server-express';
 import express from 'express'
-// import { graphqlHTTP } from 'express-graphql'
 import { buildSchema } from "type-graphql";
 import { createContext } from './context';
+import cors from "cors";
+
+require('dotenv').config()
 
 const app = express();
 
 const main = async () => {
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: process.env.IS_PROD ? "https://sails.host" : "http://localhost:3000",
+    })
+  );
 
   const schema = await buildSchema({
     emitSchemaFile: true,
@@ -19,10 +28,23 @@ const main = async () => {
   const apolloServer = new ApolloServer({
     introspection: true,
     debug: true,
-    context: ({ schema, context, req, res }) => ({ req, res }),
+    context: ({ req, res }) => ({ schema, context, req, res }),
+    formatError: (error: ApolloError) => {
+      if (error.originalError instanceof ApolloError) {
+        return error;
+      }
+      process.env.IS_PROD ? "" : console.log(error);
+      return new ApolloError("Internal server error", "INTERNAL_SERVER_ERROR");
+    }
   })
+
+  apolloServer.applyMiddleware({
+    app,
+    path: process.env.IS_PROD ? "/graphql" : "/api/graphql",
+    cors: false
+  });
   
-  app.listen(4000, () => console.log("🚀 Server is online and ready @ http://localhost:4000/gql"));
+  app.listen(4000, () => console.log(`🚀 Server is online and ready @ http://localhost:4000/${process.env.IS_PROD ? "graphql" : "api/graphql"}`));
 }
 
 main().catch((err) => console.error(err));
---
+++ proxy/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	fmt.Println("Hello world")
+}
