9e500431d51d95f78f5dad519eb77120c68027c5
Kyle <k00benne@gmail.com>
2021-03-20 20:46:06 -0400

    Add env variables

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo IP=localhost >> ./gql/.env
-echo DATABASE_URL=$DATABASE_URL >> ./gql/.env
-echo NODE_ENV=production >> ./gql/.env
-echo SENDGRID_API_KEY=$SENDGRID_API_KEY >> ./gql/.env
+echo IP=localhost >> .env
+echo DATABASE_URL=$DATABASE_URL >> .env
+echo NODE_ENV=production >> .env
+echo SENDGRID_API_KEY=$SENDGRID_API_KEY >> .env
